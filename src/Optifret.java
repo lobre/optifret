bbe96c315b28aa218e2ab4b04ccb30bb644f5813
Loric Brevet <loric.brevet@gmail.com>
2013-12-02 11:39:56 +0100

    Ajout optifret

---
+++ src/Optifret.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: loric
+ * Date: 02/12/13
+ * Time: 11:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Optifret {
+}
