cb247c64270513285546bc4e31525f5a200e8a9e
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2013-12-02 14:21:28 +0100

    Imported examples (now working).

--- src/Optifret.java
+++ src/Optifret.java
@@ -1,9 +1,7 @@
-/**
- * Created with IntelliJ IDEA.
- * User: loric
- * Date: 02/12/13
- * Time: 11:39
- * To change this template use File | Settings | File Templates.
- */
 public class Optifret {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+    }
+
 }
